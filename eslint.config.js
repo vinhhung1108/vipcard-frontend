8f44321795e5f53768861c5b22c31c914a291d9e
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2025-05-01 22:24:22 +0700

    fix error

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,37 @@
 // eslint.config.js
+
 import js from '@eslint/js';
-import next from 'eslint-plugin-next';
-import prettier from 'eslint-config-prettier';
+import pluginNext from '@next/eslint-plugin-next';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
 
 export default [
-  js.configs.recommended,
   {
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
+      parser: tseslint.parser,
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
         project: './tsconfig.json',
       },
     },
     plugins: {
-      next,
+      '@next/next': pluginNext,
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      '@typescript-eslint': tseslint.plugin,
     },
     rules: {
-      'no-console': 'warn',
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      ...js.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      ...pluginNext.configs.recommended.rules,
+      ...tseslint.configs.recommended[0].rules,
     },
   },
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: await import('@typescript-eslint/parser'),
-    },
-    plugins: {
-      '@typescript-eslint': await import('@typescript-eslint/eslint-plugin'),
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': ['warn'],
-    },
-  },
-  prettier, // Luôn để sau cùng
 ];
