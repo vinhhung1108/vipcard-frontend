2e2b446390267094324b1727da96271cb192e8a5
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2025-05-01 15:55:23 +0700

    fix error

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,26 @@
-// eslint.config.js
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import next from 'eslint-plugin-next';
+
 export default [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...ts.configs.recommendedTypeChecked,
   {
     files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': ts.plugin,
+      next,
+    },
     languageOptions: {
-      parser: '@typescript-eslint/parser',
       parserOptions: {
         project: './tsconfig.eslint.json',
         tsconfigRootDir: __dirname,
       },
     },
-    plugins: {
-      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
-      prettier: require('eslint-plugin-prettier'),
-      next: require('eslint-plugin-next'),
-    },
     rules: {
-      'prettier/prettier': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/no-explicit-any': 'off',
     },
-  },
-  {
-    ignores: ['node_modules', '.next', 'dist'],
   },
 ];
