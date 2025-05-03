0e4538bf89ebd186d74227d4fe1b37eb5956d8ad
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2025-05-03 15:18:27 +0700

    card types

--- src/types/card.ts
+++ src/types/card.ts
@@ -1,4 +1,8 @@
 export interface CardResponseDto {
+  code: string;
+  expireAt: string | number | Date;
+  remainingValue: number;
+  value: number;
   id: string;
   name: string;
   description: string;
