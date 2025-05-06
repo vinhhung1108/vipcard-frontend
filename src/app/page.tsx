caf2b1d24f198a72c61811991a136eb5839eb5d4
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2025-05-06 13:11:52 +0700

    add tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Hello Start
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <div>Footer ga</div>
-      </footer>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to VIP Card</h1>
     </div>
   );
 }
